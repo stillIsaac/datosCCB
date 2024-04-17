df0083f361ed56909614a038fbd2267a6c6e0a79
stillIsaac <isaacjanica@gmail.com>
2024-04-17 15:53:17 -0500

    added some comments to make it easier to read

--- project_booth.asm
+++ project_booth.asm
@@ -1,62 +1,63 @@
 load_a_q0:
-	MOV  A, 0
-	MOV  Q0, 0
+	MOV		A, 0 		#cargar el valor 00000000 en A, se toma por defecto que al cargar 0 se cargan los 8 bits en 0 de la variable 
+	MOV  	Q0, 0	  	#cargar el valor 00000000 en Q0, se toma por defecto que al cargar 0 se cargan los 8 bits en 0 de la variable
 
 load_q1:
-MOV  ACC, CTE
+	MOV		ACC, CTE	#cargar la dirección de Q1 en ACC
 	Q1
-	MOV  DPTR, ACC
-	MOV  ACC, [DPTR]
-	MOV  Q1, ACC
+	MOV  	DPTR, ACC	# mover la dirección de Q1 al DPTR
+	MOV  	ACC, [DPTR]	#Cargar el valor de Q1 en ACC
+	MOV  	Q1, ACC		#poner el valor de Q1 en la variable Q1
 
 
 load_multiplicand:
-	MOV  ACC, CTE
-	MULTIPLICAND
-	MOV  DPTR, ACC
-	MOV  ACC, [DPTR]
-	MOV  M, ACC
+	MOV  	ACC, CTE	#cargar la dirección de MULTIPLICAND en ACC
+	MULTIPLICAND		
+	MOV  	DPTR, ACC	# mover la dirección de MULTIPLICAND al DPTR
+	MOV  	ACC, [DPTR]	#Cargar el valor de MULTIPLICAND en ACC
+	MOV  	M, ACC		#poner el valor de MULTIPLICAND en la variable Q1
 
 load_multiplier:
-	MOV  ACC, CTE
-	MULTIPLIER
-	MOV  DPTR, ACC
-	MOV  ACC, [DPTR]
-	MOV  Q, ACC
+	MOV  	ACC, CTE	#cargar la dirección de MULTIPLIER en ACC
+	MULTIPLIER  	
+	MOV	    DPTR, ACC  	# mover la dirección de MULTIPLIER al DPTR
+	MOV  	ACC, [DPTR] #Cargar el valor de MULTIPLIER en ACC
+	MOV  	Q, ACC  	#poner el valor de MULTIPLIER en la variable Q1
 
 load_count:
-	MOV  ACC, CTE
+	MOV  	ACC, CTE	#cargar la dirección de N en ACC
 	N
-	MOV  DPTR, ACC
-	MOV  ACC, [DPTR]
-	MOV  COUNT, ACC
+	MOV  	DPTR, ACC	# mover la dirección de N al DPTR
+	MOV  	ACC, [DPTR]	#Cargar el valor de N en ACC
+	MOV  	COUNT, ACC	#poner el valor de N en la variable Q1
+
 WHILE_N:
 IF_Q1_Q0:
-	MOV X, Q0 			// X Es una variable auxiliar que va a almacenar 
-	XOR X, Q1
-	CMP X, 1
+	MOV 	X, Q0 			// X Es una variable auxiliar que va a almacenar 
+	XOR 	X, Q1
+	CMP 	X, 1
 	COND1
 COND2:
 	SHIFT_RIGHT	
 
 
 COND1:
-	MOV Y, Q0
-	AND Y, 1
-	MOV ACC, Y
+	MOV 	Y, Q0
+	AND 	Y, 1
+	MOV 	ACC, Y
 	JN
 	SUM_AM
 	
  	
 SUM_AM:
-	ADD A, M
+	ADD 	A, M
 	
 SHIFT_RIGHT:
-	SHIFTR Q, A
-	SHIFTR Q0, Q 
+	SHIFTR 	Q, A
+	SHIFTR 	Q0, Q 
 REDUCE_COUNTER:	
-	ADD N, -1
-	MOV ACC, N
+	ADD 	N, -1
+	MOV 	ACC, N
 	JN
 	WHILE_N
 	JMP CTE
@@ -64,8 +65,8 @@ REDUCE_COUNTER:
 
 
 END_LOOP:
-	CONCAT A,Q
-	MOV ACC, A
+	CONCAT 	A,Q
+	MOV 	ACC, A
 	RET
 	
 
